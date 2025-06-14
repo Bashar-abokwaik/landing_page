df4456d1dadaa53f013ba435b1503f0840076739
Bashar-abokwaik <basharabokaik@gmail.com>
2025-06-14 18:58:34 +0300

    responsev

--- landing-page-project/src/componants/Header/Header.jsx
+++ landing-page-project/src/componants/Header/Header.jsx
@@ -1,28 +1,29 @@
+import { useState } from "react";
 import logoImge from "../../assets/logo.png";
 import "./Header.css";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="heder">
       <div className="logo">
         <img src={logoImge} alt="Logo" className="logo-img" />
         <h1 className="heder-titel">{"Astraeus Gym".toUpperCase()}</h1>
       </div>
-      <nav className="nav">
+
+      <div className={`hamburger ${menuOpen ? "open" : ""}`} onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <nav className={`nav ${menuOpen ? "show" : ""}`}>
         <ul className="lest">
-          <li>
-          
-            <a href="#hero">{"Home".toUpperCase()}</a>
-          </li>
-          <li>
-            <a href="#about">{"About Us".toUpperCase()}</a>
-          </li>
-          <li>
-            <a href="#services">{"Services".toUpperCase()}</a>
-          </li>
-          <li>
-            <a href="#contact">{"Contact Us".toUpperCase()}</a>
-          </li>
+          <li><a href="#hero">HOME</a></li>
+          <li><a href="#about">ABOUT US</a></li>
+          <li><a href="#services">SERVICES</a></li>
+          <li><a href="#contact">CONTACT US</a></li>
         </ul>
       </nav>
     </header>
--- landing-page-project/src/componants/Services/ServiceCard.jsx
+++ landing-page-project/src/componants/Services/ServiceCard.jsx
@@ -3,18 +3,33 @@ import "./ServiceCard.css";
 
 function ServiceCard({ img, title, description }) {
   const [isHovered, setIsHovered] = useState(false);
+  const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
   function handleHover() {
+    if(!isTouchDevice){
     const card = document.querySelector(".card");
     card.classList.toggle("hovered");
     setIsHovered(true);
+    }
   }
+
   function handleMouseLeave() {
+    if(!isTouchDevice){
     const card = document.querySelector(".card");
     card.classList.remove("hovered");
     setIsHovered(false);
+}
+  }
+  function handleClick() {
+    if (isTouchDevice) {
+      const card = document.querySelector(".card");
+      const isCurrentlyHovered = card.classList.contains("hovered");
+      card.classList.toggle("hovered");
+      setIsHovered(!isCurrentlyHovered);
+    }
   }
   return (
-    <div onMouseOver={handleHover} onMouseLeave={handleMouseLeave} className="card">
+    <div onMouseOver={handleHover} onMouseLeave={handleMouseLeave} onClick={handleClick} className="card">
       <div className="card-image">
         <img src={img} alt={title} />
         <h3>{title}</h3>
